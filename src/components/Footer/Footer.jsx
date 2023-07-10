f3a6e0b1d5dba9c94255987fdcfbf84d09f32cdd
Axelm3sh <Axelm3sh@users.noreply.github.com>
2023-07-09 20:40:12 -0700

    port footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+// Footer.jsx
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer>
+            {/* Any code related to the Footer */}
+            <a id="backToTop" className="border rounded" href="#sidebar"><span className="fa fa-angle-double-up"></span></a>
+        </footer>
+    );
+}
+export default Footer;
