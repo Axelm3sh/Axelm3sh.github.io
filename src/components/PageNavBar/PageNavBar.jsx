9b86209a82ad9ebe8bffc146dad888cc6f191dfd
Axelm3sh <Axelm3sh@users.noreply.github.com>
2023-08-09 00:02:53 -0700

    Update GitHub Actions workflow and adjust page label and logo

--- src/components/PageNavBar/PageNavBar.jsx
+++ src/components/PageNavBar/PageNavBar.jsx
@@ -25,7 +25,7 @@ function PageNavBar({ activeIndex, onLinkSelect, navItems }) {
               <Navbar.Brand>
                   <AcmeLogo />
                   <Text b color="inherit" hideIn="xs" transform={"uppercase"}>
-                      My Page
+                      Daniel Phan
                   </Text>
               </Navbar.Brand>
               <Navbar.Content enableCursorHighlight hideIn="xs" variant="underline">
@@ -92,7 +92,7 @@ const AcmeLogo = () => (
         <rect fill="var(--secondary)" height="100%" rx="16" width="100%" />
         <path
             clipRule="evenodd"
-            d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
+            d="M17.6482 10.1305L15.8785 7.02583L5.02979 28.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
             fill="currentColor"
             fillRule="evenodd"
         />
